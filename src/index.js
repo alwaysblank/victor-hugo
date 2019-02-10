b03d52f1f0b2f51742e043bd2d00b87705aaa3cb
Ben Martinez-Bateman <ben@alwaysblank.org>
2019-02-10 13:22:27 -0800

    🗑

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // JS Goes here - ES6 supported
 
 import "./css/main.css";
+import "./js/hello";
 
 // Say hello
-console.log("🦊 Hello! Edit me in src/index.js");
+console.log(`🦊 Hello! Edit me in src/index.js`);
