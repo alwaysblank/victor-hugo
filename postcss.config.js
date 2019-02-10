c1e67975061aeffc6321b114bb2af203d7cc7451
Ben Martinez-Bateman <ben@alwaysblank.org>
2019-02-10 13:33:20 -0800

    Implement tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,15 @@
-module.exports = {
-  plugins: {
-    "postcss-import": {},
-    "postcss-preset-env": {
-      browsers: `last 2 versions`,
-    },
-    autoprefixer: {},
-  },
+const postcssPresetEnv = require(`postcss-preset-env`);
+const tailwindcss = require(`tailwindcss`);
+const autoprefixer = require(`autoprefixer`);
+
+module.exports = () => {
+  return {
+    plugins: [
+      tailwindcss(`./tailwind.config.js`),
+      postcssPresetEnv({
+        browsers: `last 2 versions`,
+      }),
+      autoprefixer(),
+    ],
+  };
 };
